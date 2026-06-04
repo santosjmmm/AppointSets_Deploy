fd5589695776da662c2512c49f7d33c1d859e1b0
santosjmmm <112379601+santosjmmm@users.noreply.github.com>
2026-06-04 13:34:09 +0800

    Update login.php

--- Backend/api/login.php
+++ Backend/api/login.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 
-header("Access-Control-Allow-Origin: http://localhost:5184");
+header("Access-Control-Allow-Origin: https://appoint-sets-deploy.vercel.app/");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");

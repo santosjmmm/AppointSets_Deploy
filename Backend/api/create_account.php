0f913f2624f8b8bec3a79a1dd4f5a3366a9dafe9
santosjmmm <112379601+santosjmmm@users.noreply.github.com>
2026-06-05 01:30:20 +0800

    Update create_account.php

--- Backend/api/create_account.php
+++ Backend/api/create_account.php
@@ -1,11 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5184");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
-
-$conn = new mysqli("localhost", "root", "", "db_appsets");
+include 'db.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 

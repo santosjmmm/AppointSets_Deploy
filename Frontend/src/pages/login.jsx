310ac8d81c1c2754b80108e07a5888e9292ef69a
santosjmmm <112379601+santosjmmm@users.noreply.github.com>
2026-06-04 13:28:09 +0800

    Update login.jsx

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch("https://appointsetsdeploy-production.up.railway.app/login.php", {
+      const res = await fetch("https://appointsetsdeploy-production.up.railway.app/Backend/api/login.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),

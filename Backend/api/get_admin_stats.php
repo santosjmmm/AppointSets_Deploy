06d43bb16876358cfda2da9842259cae228c8fd8
santosjmmm <112379601+santosjmmm@users.noreply.github.com>
2026-06-05 01:21:58 +0800

    Update get_admin_stats.php

--- Backend/api/get_admin_stats.php
+++ Backend/api/get_admin_stats.php
@@ -1,5 +1,17 @@
 <?php
-include 'db.php';
+// Disable showing errors as HTML to prevent breaking React JSON parsing
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
+header("Access-Control-Allow-Origin: https://appoint-sets-deploy.vercel.app");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
 session_start();
 
 // Custom error handler to send PHP issues as JSON instead of crashing HTML style
